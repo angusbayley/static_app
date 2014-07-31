de18256ddeb4ea536e343f5c4a130525e9410b2a
angusbayley <angusbayley@gmail.com>
2014-07-31 22:29:09 +0100

    completes user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name		"Angoose Testual"
+		email		"gossey@testual.com"
+		password	"foobar"
+		password_confirmation "foobar"
+	end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,9 +6,39 @@ describe "UserPages" do
 
   describe "Signup page" do
   	before { visit signup_path }
-  	it { should have_title('Sign Up') }
   	it { should have_content('sign up') }
   	it { should have_content('disciple') }
   	it { should_not have_content('desciple') }
   end
+
+  describe "Profile Page" do
+  	let(:user) { FactoryGirl.create(:user) }
+  	before { visit user_path(user) }
+  	it { should have_content(user.name) }
+  	it { should have_title(user.name) }
+  end
+
+  describe "signup" do
+    before { visit signup_path }
+    
+    describe "with invalid details" do
+      it "should not create a user" do
+        expect { click_button "Create account" }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid details" do
+      before do
+        fill_in "Name",               with: "Example User"
+        fill_in "Email",              with: "example@foobar.com"
+        fill_in "Password",           with: "foobar"
+        fill_in "Confirmation",  with: "foobar"
+      end
+      it "should create a user" do
+        expect { click_button "Create account" }.to change(User, :count).by(1)
+      end
+    end
+
+  end
+
 end
